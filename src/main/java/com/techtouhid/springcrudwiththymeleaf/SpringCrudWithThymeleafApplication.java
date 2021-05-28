c08e593564ab987738139368d07e3f9cdaf0e743
Touhidalamin <touhidalamin123@gmail.com>
2021-05-28 16:29:25 -0700

    Created project

---
+++ src/main/java/com/techtouhid/springcrudwiththymeleaf/SpringCrudWithThymeleafApplication.java
@@ -0,0 +1,13 @@
+package com.techtouhid.springcrudwiththymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCrudWithThymeleafApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCrudWithThymeleafApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/techtouhid/springcrudwiththymeleaf/SpringCrudWithThymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techtouhid.springcrudwiththymeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCrudWithThymeleafApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
